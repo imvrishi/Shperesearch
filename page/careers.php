7aa08f920cc555ff925e5a36000e232a963971fd
Riskey <riskey.it@gmail.com>
2018-02-02 20:47:35 +0530

    Updated images

---
+++ page/careers.php
@@ -0,0 +1,34 @@
+<div class ="contact-top-banner">
+	<div class="career-img"></div>
+	<div class="career-container">
+		<div class="row">
+			<div class="col-sm-12">
+				<div class="career-content">We offer a professional work environment with excellent potential for career development. Competitive salaries will commensurate with experience. Spheresearch is a growing company with a high standard of excellence. We work in a fun working environment and welcome people with an entrepreneurial spirit.</div>
+				<h3 class="career-title">CURRENT JOB OPPORTUNITIES</h3>
+				
+				<div class="table-wrapper">
+					<div class="table-header">
+						<div class="table-row">
+							<div class="table-data">Date</div>
+							<div class="table-data">Hiring for</div>
+						</div>
+					</div>
+					<div class="table-body">
+						<div class="table-row">
+							<div class="table-data">Jan 2018</div>
+							<div class="table-data">Senior Research Analyst</div>
+							<div class="table-data"><a href="#">More →</a></div>
+						</div>
+						<div class="table-row">
+							<div class="table-data">Jan 2018</div>
+							<div class="table-data">Business Development Manager</div>
+							<div class="table-data"><a href="#">More →</a></div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+
+<?php include( '../common/newsletter.php' ); ?>
